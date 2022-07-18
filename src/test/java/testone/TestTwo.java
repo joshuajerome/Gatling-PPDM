45f6706d0fc64b928ca86b42b381bd6b148a5cca
Joshua Jerome <joshua.jerome@gmail.com>
2022-07-18 18:23:39 -0500

    worked on TestTwo

--- src/test/java/testone/TestTwo.java
+++ src/test/java/testone/TestTwo.java
@@ -1,5 +1,28 @@
 package testone;
 
-public class TestTwo {
+/* Import Statements */
 
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import java.util.List;
+
+
+
+public class TestTwo extends Simulation {
+	
+	/* MISC */
+	
+	private String getDataFile() {
+		String datafile = System.getProperty("datafile");
+		
+		if (datafile == null || datafile.length() == 0) {
+			datafile = "data.csv";
+		}
+		return datafile;
+	}
+	
 }
