2b54328e63f8afefafcc13beb0026a838282ed83
Xia <Edward.Xia@Dell.com>
2022-07-21 21:30:20 -0700

    stream instead of for loop

--- src/test/java/testone/TestTwo.java
+++ src/test/java/testone/TestTwo.java
@@ -12,6 +12,7 @@ import io.gatling.javaapi.core.PopulationBuilder.*;
 
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class TestTwo extends Simulation {
 	
@@ -41,15 +42,81 @@ public class TestTwo extends Simulation {
 	private ChainBuilder post;
 	private ChainBuilder login;
 	private ScenarioBuilder scn;
-	PopulationBuilder[] scnList = new PopulationBuilder[6];
+	//PopulationBuilder[] scnList = new PopulationBuilder[6];
+	private List<PopulationBuilder> scnList= new ArrayList<>();
 	
 	@SuppressWarnings("unchecked")
 	private void testIterate() {
 		
-		int index = 0;
 		
-		for(TestSuite ts : tests) {
-			System.out.println(ts.id);
+		tests.stream().forEach(ts -> {
+			
+			httpProtocol = HttpDsl.http
+					.baseUrl("https://" + ts.ip)
+					.acceptHeader("application/json")
+					.userAgentHeader("Gatling Performance Test");
+			
+			switch(ts.HTTPmethod) {
+				case GET:
+					login = exec(
+			    			http("login request")
+			    			.post(":8443/api/v2/login")
+			    			.header("content-type","application/json")
+			    			.body(StringBody(credentials))
+			    			//.check(status().is(200))
+			    			.check(jmesPath("access_token").ofString().saveAs("access_token"))
+			    		    .check(jmesPath("token_type").ofString().saveAs("token_type"))
+			    			 );
+					
+					get = exec(http("HTTP Request: GET")
+							.get(ts.uri.toString())
+							.header("Authorization", session -> session.getString("token_type") 
+									+ " " + session.getString("access_token"))
+							);
+					scn = scenario("Test Suite # " 
+									+ ts.id + ":: " 
+									+ ts.HTTPmethod
+									+ " "
+									+ ts.uri.toString())
+									.exec(login,get);
+					scnList.add(scn.injectOpen(constantUsersPerSec(1).during(java.time.Duration.ofSeconds(1)))
+			         .protocols(httpProtocol));
+					
+					break;
+					
+				case POST:
+					login = exec(
+			    			http("login request")
+			    			.post(":8443/api/v2/login")
+			    			.header("content-type","application/json")
+			    			.body(StringBody(credentials))
+			    			//.check(status().is(200))
+			    			.check(jmesPath("access_token").ofString().saveAs("access_token"))
+			    		    .check(jmesPath("token_type").ofString().saveAs("token_type"))
+			    			 );
+					
+					post = exec(http("HTTP Request: POST")
+							.post(":" + ts.port + ts.restApiUri)
+							.header("Authorization", session -> session.getString("token_type") 
+									+ " " + session.getString("access_token"))
+							.body(RawFileBody("postBody.json")).asJson()
+							);
+					scn = scenario("Test Suite # " 
+							+ ts.id + ":: " 
+							+ ts.HTTPmethod
+							+ " "
+							+ ts.uri.toString())
+							.exec(login,post);
+					scnList.add(scn.injectOpen(constantUsersPerSec(1).during(java.time.Duration.ofSeconds(1)))
+			         .protocols(httpProtocol));
+					
+					break;
+			}
+			
+		});
+		//int index = 0;
+		
+	/*	for(TestSuite ts : tests) {
 			
 			httpProtocol = HttpDsl.http
 					.baseUrl("https://" + ts.ip)
@@ -74,6 +141,7 @@ public class TestTwo extends Simulation {
 							.header("Authorization", session -> session.getString("token_type") 
 									+ " " + session.getString("access_token"))
 							);
+					
 					scn = scenario("Test Suite " + ts.id + ": " + "GET request").exec(login,get);
 					scnList[index] = scn.injectOpen(constantUsersPerSec(1).during(java.time.Duration.ofSeconds(1)))
 			         .protocols(httpProtocol);
@@ -104,7 +172,7 @@ public class TestTwo extends Simulation {
 					index++;
 					break;
 			}
-		}
+		}*/
 	}
 	
 	{
