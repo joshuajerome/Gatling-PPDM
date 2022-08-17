1414ada497e7e3bd561a8945ef0758619c56a058
Jerome <Joshua.Jerome@Dell.com>
2022-08-17 13:56:24 -0700

    working POST of POST

--- src/test/java/testone/TestSuite.java
+++ src/test/java/testone/TestSuite.java
@@ -1,105 +1,121 @@
 package testone;
 
+import java.awt.datatransfer.SystemFlavorMap;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Base64;
+
+
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+
+
 /* Can incorporate several HTTP Verbs */
 enum HTTPMethod {
-	GET,
-	POST;
+    GET,
+    POST,
+    POSTPOST;
 }
 
-/* TestSuite Class 
- * Decodes PostBody given as Base64 encryption
- * Constructs TestSuite Object with unique CSV fields
- */
+
+
+/* TestSuite Class
+* Decodes PostBody given as Base64 encryption
+* Constructs TestSuite Object with unique CSV fields
+*/
+
+
 
 public class TestSuite {
-	
-	 int id;
-	 String restApiUri;
-	 int port;
-	 HTTPMethod HTTPmethod;
-	 int requestCount;
-	 int threadCount;
-	 String requestBody;
-	 int testDuration;
-	 String ip;
-	 URI uri;
-	 
-	TestSuite (int id, String restApiUri, int port, HTTPMethod method, int requestCount, int threadCount, String body, int testDuration, String ip){
-		this.id = id;
-		this.restApiUri = restApiUri;
-		this.port = port;
-		this.ip = ip;
-		this.requestCount = requestCount;
-		this.threadCount = threadCount;
-		this.HTTPmethod = method;
-		this.testDuration = testDuration;
-		
-		/* Get base URI*/
-		try {
-			this.uri = new URI("https://" + this.ip + ":" + this.port + this.restApiUri);	
-			
-			 /* Base 64 Encryption of Post Body
-			  * 
-		eyJuYXR1cmFsSWRzIjogWyI3N3o5MjI3ZC05NjRhLTQwNjktYTk1OC04MWI0OTcxYTA0MzU5Il0sIm5hbWUiOiAiQXBwbGljYXRpb24gSG9zdCAxIiwidHlwZSI6ICJHRU5FUklDX0FQUExJQ0FUSU9OX0hPU1QiLCJjYXRlZ29yaWVzIjogWyJBUFBMSUNBVElPTl9IT1NUIiwiQVBQTElDQVRJT05fU1lTVEVNIl0sInZlbmRvciI6ICJHRU5FUklDIiwidmVyc2lvbiI6ICJjdXJyZW50IHZlcnNpb24iLCJkaXNjb3ZlcmVkQXQiOiAiMjAyMS0wMy0yOVQyMzowNDowMS4wMDdaIiwiZGlzY292ZXJ5U3RhdHVzIjogIkFWQUlMQUJMRSIsImhvc3RuYW1lIjoidGVzdC5hc2wubGFiLmVtYy5jb20iLCJhZGRyZXNzZXMiOiBbeyJ2YWx1ZSI6ICIxMC4yNS4xNDUuMTIiLCJ0eXBlIjogIklQVjQifV0sImF2YWlsYWJpbGl0eVN0YXR1cyI6IHsidmFsdWUiOiAiQVZBSUxBQkxFIiwiY2hhbmdlZEF0IjogbnVsbH0sImFnZW50UmVmIjogeyJpZCI6ICI3N3o5MjI3ZC05NjRhLTQwNjktYTk1OC04MWI0OTcxYTA0Nzc4In19
-			  * 
-			  */
-			
-			/* Given Base 64 Encyption of Post Body, TODO this:
-			 * byte[] decodedBytes = Base64.getDecoder().decode(body);
-			 * this.requestBody = new String(decodedBytes);
-			 * 
-			 * If implementing this approach:
-			 * make sure to change the postBody field in data.csv to Base64 encryption
-			 */
-			
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-	 	}
+    
+     int id;
+     String restApiUri;
+     int port;
+     HTTPMethod HTTPmethod;
+     int requestCount;
+     int threadCount;
+     ArrayList<String> requestBody = new ArrayList<String>();
+     int testDuration;
+     String ip;
+     URI uri;
+     
+    @SuppressWarnings("null")
+    TestSuite (int id, String restApiUri, int port, HTTPMethod method, int requestCount, int threadCount, String body, int testDuration, String ip){
+        this.id = id;
+        this.restApiUri = restApiUri;
+        this.port = port;
+        this.ip = ip;
+        this.requestCount = requestCount;
+        this.threadCount = threadCount;
+        this.HTTPmethod = method;
+        this.testDuration = testDuration;
+        
+        /* Get base URI*/
+        try {
+            this.uri = new URI("https://" + this.ip + ":" + this.port + this.restApiUri);    
+            
+             /* Base 64 Encryption of Post Body
+              *
+        eyJuYXR1cmFsSWRzIjogWyI3N3o5MjI3ZC05NjRhLTQwNjktYTk1OC04MWI0OTcxYTA0MzU5Il0sIm5hbWUiOiAiQXBwbGljYXRpb24gSG9zdCAxIiwidHlwZSI6ICJHRU5FUklDX0FQUExJQ0FUSU9OX0hPU1QiLCJjYXRlZ29yaWVzIjogWyJBUFBMSUNBVElPTl9IT1NUIiwiQVBQTElDQVRJT05fU1lTVEVNIl0sInZlbmRvciI6ICJHRU5FUklDIiwidmVyc2lvbiI6ICJjdXJyZW50IHZlcnNpb24iLCJkaXNjb3ZlcmVkQXQiOiAiMjAyMS0wMy0yOVQyMzowNDowMS4wMDdaIiwiZGlzY292ZXJ5U3RhdHVzIjogIkFWQUlMQUJMRSIsImhvc3RuYW1lIjoidGVzdC5hc2wubGFiLmVtYy5jb20iLCJhZGRyZXNzZXMiOiBbeyJ2YWx1ZSI6ICIxMC4yNS4xNDUuMTIiLCJ0eXBlIjogIklQVjQifV0sImF2YWlsYWJpbGl0eVN0YXR1cyI6IHsidmFsdWUiOiAiQVZBSUxBQkxFIiwiY2hhbmdlZEF0IjogbnVsbH0sImFnZW50UmVmIjogeyJpZCI6ICI3N3o5MjI3ZC05NjRhLTQwNjktYTk1OC04MWI0OTcxYTA0Nzc4In19
+              *
+              */
+            
+            /* Given Base 64 Encyption of Post Body, TODO this:
+             * byte[] decodedBytes = Base64.getDecoder().decode(body);
+             * this.requestBody = new String(decodedBytes);
+             *
+             * If implementing this approach:
+             * make sure to change the postBody field in data.csv to Base64 encryption
+             */
+            
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+         }
 
-		/* Get postBody via location passed via csv */
-		try {
-			StringBuilder sb = new StringBuilder();
-			ClassLoader cl = Thread.currentThread().getContextClassLoader();
-			InputStream  is = cl.getResourceAsStream(body);	
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			String read;
-			try {
-				while((read = br.readLine()) != null) {
-					sb.append(read);
-				}
-			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-			this.requestBody = sb.toString();
-			//System.out.println("TEST" + this.requestBody);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+       /* Get postBody via location passed via csv */
+        try {
+            String[] numBodies = body.split("/");
+            for(int i = 0; i < numBodies.length; i++) {
+                
+                StringBuilder sb = new StringBuilder();
+                ClassLoader cl = Thread.currentThread().getContextClassLoader();
+                InputStream  is = cl.getResourceAsStream(numBodies[i]);    
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String read;
+                try {
+                    while((read = br.readLine()) != null){
+                        sb.append(read);
+                    }
+                    this.requestBody.add(sb.toString());
+                }
+                catch(Exception e) {
+                        e.printStackTrace();
+                }
+                }
+            }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+   }
 
-	}
 
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append("Test Suite #" + this.id + "\n");
-		sb.append("Rest Api Uri: " + this.uri.toString() + "\n");
-		sb.append("Request Count:" + requestCount + "\n");
-		sb.append("Thread Count:" + threadCount + "\n");	
-		sb.append("Method:" + HTTPmethod.toString() + "\n");
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		sb.append("Request Body:" + gson.toJson(requestBody) + "\n");
-		return sb.toString();
-	}
+
+   @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Test Suite #" + this.id + "\n");
+        sb.append("Rest Api Uri: " + this.uri.toString() + "\n");
+        sb.append("Request Count:" + requestCount + "\n");
+        sb.append("Thread Count:" + threadCount + "\n");    
+        sb.append("Method:" + HTTPmethod.toString() + "\n");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        sb.append("Request Body:" + gson.toJson(requestBody) + "\n");
+        return sb.toString();
+    }
 }
--- src/test/java/testone/Testing.java
+++ src/test/java/testone/Testing.java
@@ -20,6 +20,8 @@ import java.util.Collections;
 import java.util.stream.*;
 
 /** TODO: 
+ * Post of the Post
+ * 
  * 
  */
 
@@ -34,8 +36,10 @@ public class Testing extends Simulation {
 	/* Post Body Variables */
 	private String access_token = null;
 	private String token_type = "";
+	private String secondPostID = null;
 	private String credentials;
-	private final AtomicInteger counter = new AtomicInteger(1);	
+	private final AtomicInteger appHostCounter = new AtomicInteger(1);	
+	private final AtomicInteger appSysCounter = new AtomicInteger(1);	
 	private final AtomicInteger loginCounter = new AtomicInteger(1);	
 	
 	/* Builders */
@@ -82,21 +86,27 @@ public class Testing extends Simulation {
 			Map<?, ?> map = gson.fromJson(currentPost, Map.class);
 			
 			/* Creates unique agent ref ID */
-			Map<String, String> agentRef = (Map<String, String>)map.get("agentRef");
-			agentRef.put("id",UUID.randomUUID().toString());
-			
+			if (map.containsKey("agentRef")) {
+				Map<String, String> agentRef = (Map<String, String>)map.get("agentRef");
+				agentRef.put("id",UUID.randomUUID().toString());
+				/* Changes Application Host Name */
+				Map<String, String> temp = (Map<String, String>)map;
+				temp.replace("name", "Application Host " + appHostCounter.getAndIncrement());	
+			} else if (map.containsKey("applicationHostRef")) {
+				Map<String, String> agentRef = (Map<String, String>)map.get("applicationHostRef");
+				agentRef.put("id",secondPostID);
+				/* Changes Application Host Name */
+				Map<String, String> temp = (Map<String, String>)map;
+				temp.replace("name", "Application System " + appSysCounter.getAndIncrement());
+			}
 			/* Creates & replaces unique naturalId */
 			ArrayList<String> naturalIds = (ArrayList<String>)map.get("naturalIds");
 			naturalIds.remove(0);
 			naturalIds.add(UUID.randomUUID().toString());
-			
-			/* Changes Application Host Name */
-			Map<String, String> temp = (Map<String, String>)map;
-			temp.replace("name", "Application Host " + counter.getAndIncrement());			
-			
+						
 			/* Converts Java Map back to JSON File */
 			newPost = gson.toJson(map);
-			// System.out.println("Agent Id: " + agentRef.get("id") + "\n" + newPost);
+//			System.out.println("\n" + newPost + "\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -183,11 +193,10 @@ public class Testing extends Simulation {
 					
 				case POST:
 					
-					/* Makes sure that there are unique bodies for each request
-					 */
+					/* Makes sure that there are unique bodies for each request */
 					  Iterator<Map<String, Object>> bodyFeeder =
 					  Stream.generate((Supplier<Map<String, Object>>) () -> {
-					      String body = getNewPost(ts.requestBody);
+					      String body = getNewPost(ts.requestBody.get(0));
 					      return Collections.singletonMap("body", body);
 					    }
 					  ).iterator();
@@ -243,9 +252,97 @@ public class Testing extends Simulation {
 								  postScn.injectClosed(rampConcurrentUsers(1).to(ts.threadCount).during((java.time.Duration.ofSeconds(ts.testDuration))))
 							         .protocols(httpProtocol)));
 				
-				break;
-			}});
-	}
+					break;
+					
+				case POSTPOST:
+					
+					Iterator<Map<String, Object>> appHostFeeder =
+                    Stream.generate((Supplier<Map<String, Object>>) () -> {
+                        String body = getNewPost(ts.requestBody.get(0));
+                        return Collections.singletonMap("appHostBody", body);
+                      }
+                    ).iterator();
+                    
+                    Iterator<Map<String, Object>> appSysFeeder =
+                            Stream.generate((Supplier<Map<String, Object>>) () -> {
+                                String body = ts.requestBody.get(1);
+                                return Collections.singletonMap("appSysBody", body);
+                              }
+                            ).iterator();
+                    
+                    Iterator<Map<String, Object>> authTokenFeederPP =
+                            Stream.generate((Supplier<Map<String, Object>>) () -> {
+                                return Collections.singletonMap("token", access_token);
+                              }
+                            ).iterator();
+                    
+                    Iterator<Map<String, Object>> appSysBodyFeeder =
+                            Stream.generate((Supplier<Map<String, Object>>) () -> {
+                            	String appSysBody = getNewPost(ts.requestBody.get(1));
+                                return Collections.singletonMap("appSysBody", appSysBody);
+                              }
+                            ).iterator();
+                    
+					  /* Get credentials */
+					  try {
+							credentials = getCredentials();
+						} catch (Exception e) {
+							return;
+						}
+						
+					  /* Create login scenario */
+						loginScn = scenario("Login" + loginCounter.getAndIncrement())
+								.exec(
+				    			http("login request")
+				    			.post(":8443/api/v2/login")
+				    			.header("content-type","application/json")
+				    			.body(StringBody(credentials))
+				    			.check(jmesPath("access_token").ofString().saveAs("access_token"))
+				    		    .check(jmesPath("token_type").ofString().saveAs("token_type"))
+				    			 )
+								.exec(session -> {
+									access_token = session.getString("access_token");
+									token_type = session.getString("token_type");
+								    return session;
+						            });
+					
+					postScn = scenario("Test Suite # "
+                  + ts.id + "::"
+                  + ts.HTTPmethod
+                  + " "
+                  + ts.uri.toString())
+                  .repeat(ts.requestCount, "index").on(
+                           feed(appHostFeeder)
+                          .feed(appSysFeeder)
+                          .feed(authTokenFeederPP)
+                          .exec(http("HTTP Request: POST")
+                                  .post(":" + ts.port + ts.restApiUri)
+                                  .header("Authorization", "bearer"
+                                          + " " + "#{token}")
+                                  .body(StringBody("#{appHostBody}")).asJson()
+                                  .check(jmesPath("agentRef.id").saveAs("secondPostID")))
+                          .exec(session -> {
+                        	  secondPostID = session.getString("secondPostID");
+//                        	  System.out.println(secondPostID);
+                        	  return session;
+                          })
+                          .feed(appSysBodyFeeder)
+                          .exec(http("HTTP Request: 2nd POST")
+                                  .post(":" + ts.port + ts.restApiUri)
+                                  .header("Authorization", "bearer"
+                                          + " " + "#{token}")
+                                  .body(StringBody("#{appSysBody}")).asJson()
+                                  ));
+					scnList.add(loginScn.injectClosed(constantConcurrentUsers(1).during(java.time.Duration.ofSeconds(1)))
+							  .andThen(
+									  postScn.injectClosed(rampConcurrentUsers(1).to(ts.threadCount).during((java.time.Duration.ofSeconds(ts.testDuration))))
+								         .protocols(httpProtocol)));
+					
+					
+				} // end switch
+			} // end func within steam
+		); // end stream
+	} // end method
 	
 	/* Runs all scenario within PopulationBuilder list */
 	{
