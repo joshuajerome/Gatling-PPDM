ddc960dca9cb1667bb3bbe102b0520e8a8b0b991
Joshua Jerome <73908842+joshuajerome@users.noreply.github.com>
2022-08-22 12:49:46 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-@mvn gatling:test -Dgatling.simulationClass=testone.Testing -Ddatafile="test.csv" -Dusername="admin" -Dpassword="Changeme@1"
+@mvn gatling:test -Dgatling.simulationClass=testone.Testing -Ddatafile="test.csv" -Dusername="" -Dpassword=""
