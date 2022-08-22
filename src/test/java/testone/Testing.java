c31f0a19a96304af62d50bc809daadcea7e3d426
Joshua Jerome <73908842+joshuajerome@users.noreply.github.com>
2022-08-22 12:54:46 -0700

    Update Testing.java

--- src/test/java/testone/Testing.java
+++ src/test/java/testone/Testing.java
@@ -88,7 +88,7 @@ public class Testing extends Simulation {
 		loginScn = scenario("Login " + specification)
 				.exec(
     			http("login request")
-    			.post(":8443/api/v2/login")
+    			.post("**CONFIDENTAIL**")
     			.header("content-type","application/json")
     			.body(StringBody(credentials))
     			.check(jmesPath("access_token").ofString().saveAs("access_token"))
